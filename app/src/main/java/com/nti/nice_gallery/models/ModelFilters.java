f0a3a959063a07050e256b9b221970897676c92e
Артем <user@WIN-QERL5BT18CI>
2025-12-25 21:02:13 +0500

    implement: ActivityFilters

--- app/src/main/java/com/nti/nice_gallery/models/ModelFilters.java
+++ app/src/main/java/com/nti/nice_gallery/models/ModelFilters.java
@@ -2,6 +2,7 @@ package com.nti.nice_gallery.models;
 
 import androidx.annotation.Nullable;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -9,8 +10,8 @@ public class ModelFilters {
     @Nullable public List<ModelMediaTreeItem.Type> types;
     @Nullable public Long minWeight;
     @Nullable public Long maxWeight;
-    @Nullable public Date minCreateDate;
-    @Nullable public Date maxCreateDate;
+    @Nullable public LocalDate minCreateDate;
+    @Nullable public LocalDate maxCreateDate;
     @Nullable public List<String> extensions;
     @Nullable public Integer duration;
 }
